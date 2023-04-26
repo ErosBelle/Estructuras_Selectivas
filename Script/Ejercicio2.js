25df1cc7467033254e048f1cfac4416d9642884a
ErosBelle <vegetil25@gmail.com>
2023-04-26 14:28:20 -0500

    Modificacion javascript

---
+++ Script/Ejercicio2.js
@@ -0,0 +1,16 @@
+let edadh1
+
+edadh1 = Number(prompt('Ingrese la edad'))
+
+if (edadh1<10) {
+    alert('Niño')
+} else if (edadh1>10 && edadh1<15 ) {
+    alert('Preadolescente')
+}  else if (edadh1>15 && edadh1<18 ) {
+    alert('adoslecente')
+}  else if (edadh1>18 && edadh1<50 ) {
+    alert('Adulto')
+}  else if (edadh1>50) {
+    alert('Adulto Mayor')
+} 
+
